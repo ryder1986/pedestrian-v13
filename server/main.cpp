1c26495d5890f44bd02e6f32287bfc1ce9177a9f
root <you@example.com>
2017-12-08 16:34:55 +0800

    1

--- server/main.cpp
+++ server/main.cpp
@@ -1,150 +1,42 @@
-#include "test.h"
-//static int ttt=1;
-//class abcd
-
-//{
-
-//public:
-//    abcd() {}
-//   //   static int aaa;
-//    static void test_fun()
-//    {
-//      //      aaa++;
-//       //     aaa=7;
-//    }
-
-//};
-//int main()
-//{
-//    abcd aa;
-//    aa.test_fun();
-//    ttt=2;
-//  //  abcd::aaa=3;
-////     abc a;
-////    int a1=a.aaa;
-////    abc a;
-////    a.aaa=123;
-// //   Tools::aaaa=23;
-//  //  abc a;
-// //   a.test_fun();
-//   //abc::aaa=1;
-// //   abcd::aaa++;
-//    Test t;
-//    t.fun();
-//    return 0;
-//}
-#include <iostream>
-using namespace std;
-class Myclass1
-{
-private:
-    int a,b,c;
-
-public:
-    static int Sum;    //声明静态数据成员
-    Myclass1(){};
-};
-class Myclass
-{
-private:
-    int a,b,c;
-
-public:
-    static int Sum;    //声明静态数据成员
-    Myclass(int a,int b,int c);
-    void GetSum();
-};
-int Myclass::Sum=0;       //定义并初始化静态数据成员
-int Myclass1::Sum=0;       //定义并初始化静态数据成员
-Myclass::Myclass(int a,int b,int c)
-{
-    this->a=a;
-    this->b=b;
-    this->c=c;
-    Sum+=a+b+c;
-}
-void Myclass::GetSum()
-{
-    cout <<"Sum=" <<Sum <<endl;
-}
-mutex Tools::lock;
-//int main(void)
-//{
-////        Myclass me(10,20,30);
-////        me.Sum=123;
-////        Myclass1::Sum=1234;
-////        me.GetSum();
-//    Test t;
-//    t.fun111();
-//     //   system("pause");
-//        return 0;
-//}
-
-
-
-
+//#include "test.h"
 #include <QCoreApplication>
-
-
-//int log_level=3;// 1.no log  2.print log 3 print and write log 4.write without print log
-int test()
+#include "tools.h"
+#include "camera.h"
+void test_fun()
 {
-    int i=10;
-    return i;
+  prt(info,"test_fun running");
 }
-void test1(  int &t)
+void test_fun1(void * data)
 {
-    cout<<t<<endl;
+    int *p= (int *)data;
+  prt(info,"test_fun running %d",*p);
 }
-class A
-{
-
-public:
-    A()
-    {
-        QTimer *t=new QTimer;
-        thread *p=THREAD_DEF(A,fun);
-    }
-    void fun(){
-
-        cout<<"fun"<<endl;
-    }
-};
-A ssss()
-{
-    A aa;
-    return aa;
-}
-void ttss(const A &tmp)
-{
-
-}
-class Test1{
-    mutex ccmd_list_lock;
-public:
-    //       Test1(Test1 &&)
-    //       {}
-    Test1(const Test1 &){}//std:move use copy-constructor
-    Test1()=default;//constructor can be: default/delete/self-defined
-    Test1(int t){}
-
-    void test_fun()
-    {
-        std::thread  *fetch_cmd_thread=new std::thread(std::mem_fn(&Test1::fun),*this);
-        fetch_cmd_thread->join();
-    }
-
-    void fun(){
-        cout<<"fun"<<endl;
-    }
-};
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    //        Test1 t1;
-    //        t1.test_fun();
-    Test t;
-    t.fun111();
+     QCoreApplication a(argc, argv);
+    prt(info,"start main");
+
+
+
+    ServerInfoReporter *reportor=new ServerInfoReporter();
+    CameraManager *mgr=new CameraManager();
+    reportor->start();
+    this_thread::sleep_for(chrono::seconds(3));
+    delete mgr;
+
+
+ //   Timer t(test_fun,100);
+//    int tmp=123;
+//    Timer t(test_fun1,(void *)&tmp,100);
+//   t.start();
+//    this_thread::sleep_for(chrono::seconds(1));
+//    t.stop();
+//       prt(info,"pause  main for a while");
+//    this_thread::sleep_for(chrono::seconds(1));
+//    t.start();
+//    prt(info,"end main");
+//    while(1)
+//        ;
     return a.exec();
 }
